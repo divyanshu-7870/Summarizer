6bd47a28f8e29131bf92bb521bfa95536543b9fe
divyanshu-7870 <divyanshu78709@gmail.com>
2025-12-30 21:59:19 +0530

    Improve text cleaning and summary extraction process
    Refactored text cleaning and summary generation logic.

--- summarizer.py
+++ summarizer.py
@@ -1,11 +1,16 @@
-myText= "The Madras High Court has suggested to the Centre explore the possibility of passing Australia-like legislation banning social media for children under the age of 16. The Madurai bench of the High Court also directed the authorities concerned to accelerate their awareness campaign more effectively and take the message to the vulnerable group through all available media till such a law comes into effect. Australia, on December 10, became the first country in the world to ban social media for children under 16, a decision welcomed by many parents and child rights groups. A bench comprising Justice G Jayachandran and Justice KK Ramakrishnan suggested this to the Centre while disposing of a Public Interest Litigation (PIL) raising the issue of easy availability of pornographic content to children on social media and seeking a direction to the authorities to implement the direction of the Centre to the internet service providers for providing “Parental Window” service and create awareness among the children."
+myText = input("Put the Blog below :")
 splitted_sentences = myText.split(".")
-cleaned_sentences = []
-for s in splitted_sentences :
-    cleaned = s.strip()
-    if cleaned != "" :
-        cleaned_sentences.append(cleaned)
-#analysis = "The Madras High Court has suggested to the Centre explore the possibility of passing Australia-like legislation banning social media for children under the age of 16. The Madurai bench of the High Court also directed the authorities concerned to accelerate their awareness campaign more effectively and take the message to the vulnerable group through all available media till such a law comes into effect. Australia, on December 10, became the first country in the world to ban social media for children under 16, a decision welcomed by many parents and child rights groups. A bench comprising Justice G Jayachandran and Justice KK Ramakrishnan suggested this to the Centre while disposing of a Public Interest Litigation (PIL) raising the issue of easy availability of pornographic content to children on social media and seeking a direction to the authorities to implement the direction of the Centre to the internet service providers for providing “Parental Window” service and create awareness among the children."
+
+def text_cleaning (sentence): # empty spaces removal
+    cleaned = []
+    for s in sentence :
+      sentence_stripped = s.strip()
+      if sentence_stripped != "" :
+        cleaned.append(sentence_stripped)
+    return cleaned    
+
+cleaned_sentences = text_cleaning(splitted_sentences)
+
 analysis = myText.lower()
 
 signs =[",","-","(",")","'",'"',"."]
@@ -35,7 +40,7 @@ def getcount(wordcount):
 
 sorted_wordcount = sorted(worditems,key = lambda x: x[1], reverse= True)
 
-#print(cleaned_sentences)
+
 sentence_score = {}
 for s in cleaned_sentences:
     score=0
@@ -51,16 +56,25 @@ for s in cleaned_sentences:
     sentence_score[s] = score        
         
 
-summary_len = max(1, int(len(sentence_score))*0.3)
+summary_len = max(1, int(len(sentence_score)*0.3))
 
 sorted_sentence_score = sorted(sentence_score.items(),key= lambda x: x[1],reverse=True)
 
-print(sorted_sentence_score)
+top_sentences = sorted_sentence_score[:summary_len]
+
+final_summary=[]
+
+for s in cleaned_sentences:
+    for ts,_ in top_sentences:
+        if s == ts:
+            final_summary.append(s)
+
+print("\nSummary :\n")
+for s in final_summary:
+    print(s," ")
 
 
 
 
 
 
-
-#print(cleaned_sentences)
